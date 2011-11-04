b7632d5543c97b69917c0c49c7008ddba2446ed7
Dan Creswell <dan.creswell@gmail.com>
2011-11-04 14:40:03 +0000

    Release 2.1.5
    Update codebase version.

--- src/org/dancres/blitz/VersionInfo.java
+++ src/org/dancres/blitz/VersionInfo.java
@@ -22,7 +22,7 @@ public class VersionInfo {
     public static final String PRODUCT_NAME = "Blitz JavaSpaces (PureJavaEdition)";
     public static final String EMAIL_CONTACT = "blitz@dancres.org";
     public static final String SUPPLIER_NAME = "The Blitz Project";
-    public static final String VERSION = "2.1.4";
+    public static final String VERSION = "2.1.5";
 
     static {
         StatsBoard.get().add(new Generator());
