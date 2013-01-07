2aacb05f8fc4455bd4c8fbf0b258012811593854
Dan Creswell <dan.creswell@gmail.com>
2013-01-07 19:45:12 +0000

    LocalTxnManager is an implementation detail of TxnId and should be scoped accordingly.

--- src/org/dancres/blitz/txn/TxnId.java
+++ src/org/dancres/blitz/txn/TxnId.java
@@ -7,19 +7,23 @@ import java.rmi.RemoteException;
 import java.rmi.MarshalledObject;
 
 import java.util.Map;
-import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
 
-import net.jini.security.ProxyPreparer;
+import net.jini.core.lease.LeaseDeniedException;
+
+import net.jini.core.transaction.*;
 
 import net.jini.core.transaction.server.TransactionManager;
-import net.jini.core.transaction.TransactionException;
+import net.jini.core.transaction.server.CrashCountException;
+import net.jini.core.transaction.server.TransactionParticipant;
+
+
+import net.jini.security.ProxyPreparer;
 
 import net.jini.config.ConfigurationException;
 
 import org.dancres.blitz.Lifecycle;
 import org.dancres.blitz.LifecycleRegistry;
-import org.dancres.blitz.entry.OpInfo;
 
 import org.dancres.blitz.config.ConfigurationFactory;
 
@@ -80,6 +84,90 @@ public final class TxnId implements Serializable {
 
     private transient TransactionManager theManager;
     private transient boolean isPrepared;
+
+    /**
+     Provides uniformity in the way we handle transactions.  Where all
+     transactions are seen to have a manager.  Thus, all null transactions
+     have a transaction manager of type LocalTxnManager.
+     */
+    private static class LocalTxnManager implements TransactionManager, Serializable {
+
+        /**
+         * This will need updating based on reading the log or reading
+         * from checkpoint or both! NO IT WON'T we simply won't log these - they
+         * either complete or they don't - they're one operation wonders which
+         * don't need logging.
+         */
+        private long theNextNullKey = 0;
+
+        long nextId() {
+            synchronized(this) {
+                return theNextNullKey++;
+            }
+        }
+
+        public Created create(long lease)
+                throws LeaseDeniedException, RemoteException {
+
+            throw new org.dancres.util.NotImplementedException();
+        }
+
+        public void join(long id, TransactionParticipant part, long crashCount)
+                throws UnknownTransactionException, CannotJoinException,
+                CrashCountException, RemoteException {
+
+            throw new org.dancres.util.NotImplementedException();
+        }
+
+        public int getState(long id)
+                throws UnknownTransactionException, RemoteException {
+
+            throw new org.dancres.util.NotImplementedException();
+        }
+
+        public void commit(long id)
+                throws UnknownTransactionException, CannotCommitException,
+                RemoteException {
+
+            throw new org.dancres.util.NotImplementedException();
+        }
+
+        public void commit(long id, long waitFor)
+                throws UnknownTransactionException, CannotCommitException,
+                TimeoutExpiredException, RemoteException {
+
+            throw new org.dancres.util.NotImplementedException();
+        }
+
+        public void abort(long id)
+                throws UnknownTransactionException, CannotAbortException,
+                RemoteException {
+
+            throw new org.dancres.util.NotImplementedException();
+        }
+
+        public void abort(long id, long waitFor)
+                throws UnknownTransactionException, CannotAbortException,
+                TimeoutExpiredException, RemoteException {
+
+            throw new org.dancres.util.NotImplementedException();
+        }
+
+        public String toString() {
+            return "NullTxnMgr";
+        }
+
+        public int hashCode() {
+            return 1;
+        }
+
+        public boolean equals(Object anObject) {
+            if (anObject instanceof LocalTxnManager)
+                return true;
+            else
+                return false;
+        }
+    }
 
     private static final LocalTxnManager LOCAL_TXN_MGR =
         new LocalTxnManager();
