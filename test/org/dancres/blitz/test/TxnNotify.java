38ca2ad871a8cdd51c1ecc7ad5a3df7cc2159197
Dan Creswell <dan.creswell@gmail.com>
2013-01-09 17:03:38 +0000

    Make TxnNotify a junit test

--- test/org/dancres/blitz/test/TxnNotify.java
+++ test/org/dancres/blitz/test/TxnNotify.java
@@ -18,66 +18,73 @@ import org.dancres.blitz.*;
 import org.dancres.blitz.mangler.*;
 
 import org.dancres.blitz.remote.LocalTxnMgr;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 public class TxnNotify {
-    public static void main(String args[]) {
-        try {
-            SpaceImpl mySpace = new SpaceImpl(new TxnGatewayImpl());
-            EntryMangler myMangler = new EntryMangler();
+    private SpaceImpl _space;
+    private EntryMangler _mangler;
+    private LocalTxnMgr _mgr;
 
-            TestEntry myEntry = new TestEntry().init();
+    @Before
+    public void init() throws Exception {
+        _space = new SpaceImpl(new TxnGatewayImpl());
+        _mangler = new EntryMangler();
+        _mgr = new LocalTxnMgr(1, _space.getTxnControl());
+    }
+    
+    @After
+    public void deinit() throws Exception {
+        _space.stop();
+    }
+    
+    @Test
+    public void doNotify() throws Exception {
+        TestEntry myEntry = new TestEntry().init();
+        TestEntry myTemplate = new TestEntry();
 
-            TestEntry myTemplate = new TestEntry();
+        MangledEntry myPackedEntry = _mangler.mangle(myEntry);
+        MangledEntry myPackedTemplate = _mangler.mangle(myTemplate);
 
-            MangledEntry myPackedEntry = myMangler.mangle(myEntry);
-            MangledEntry myPackedTemplate = myMangler.mangle(myTemplate);
+        EventListener myNullListener = new EventListener();
+        EventListener myTxnListener = new EventListener();
 
-            EventListener myNullListener = new EventListener();
-            EventListener myTxnListener = new EventListener();
+        _space.notify(myPackedTemplate, null, myNullListener,
+                Lease.FOREVER,
+                new MarshalledObject(new Integer(12345)));
 
-            mySpace.notify(myPackedTemplate, null, myNullListener,
-                           Lease.FOREVER,
-                           new MarshalledObject(new Integer(12345)));
+        LocalTxnMgr myMgr = new LocalTxnMgr(1, _space.getTxnControl());
+        ServerTransaction myTxn = myMgr.newTxn();
 
-            LocalTxnMgr myMgr = new LocalTxnMgr(1, mySpace.getTxnControl());
-            ServerTransaction myTxn = myMgr.newTxn();
+        _space.notify(myPackedTemplate, myTxn, myTxnListener,
+                Lease.FOREVER,
+                new MarshalledObject(new Integer(67890)));
 
-            mySpace.notify(myPackedTemplate, myTxn, myTxnListener,
-                           Lease.FOREVER,
-                           new MarshalledObject(new Integer(67890)));
+        // Will be caught only by txn listener
+        //
+        _space.write(myPackedEntry, myTxn, Lease.FOREVER);
 
-            // Will be caught only by txn listener
-            //
-            mySpace.write(myPackedEntry, myTxn, Lease.FOREVER);
+        Assert.assertEquals(1, myTxnListener.waitOnCount(1, 500));
 
-            Assert.assertEquals(1, myTxnListener.waitOnCount(1, 500));
+        // Will be caught by both listeners
+        //
+        _space.write(myPackedEntry, null, Lease.FOREVER);
 
-            // Will be caught by both listeners
-            //
-            mySpace.write(myPackedEntry, null, Lease.FOREVER);
+        Assert.assertEquals(2, myTxnListener.waitOnCount(2, 500));
+        Assert.assertEquals(1, myNullListener.waitOnCount(1, 500));
 
-            Assert.assertEquals(2, myTxnListener.waitOnCount(2, 500));
-            Assert.assertEquals(1, myNullListener.waitOnCount(1, 500));
+        // Txn listener will now close out with the transaction, null sees another entry
+        //
+        myTxn.commit();
 
-            // Txn listener will now close out with the transaction, null sees another entry
-            //
-            myTxn.commit();
+        Assert.assertEquals(2, myNullListener.waitOnCount(2, 500));
 
-            Assert.assertEquals(2, myNullListener.waitOnCount(2, 500));
+        // Will only be seen by null listener
+        //
+        _space.write(myPackedEntry, null, Lease.FOREVER);
 
-            // Will only be seen by null listener
-            //
-            mySpace.write(myPackedEntry, null, Lease.FOREVER);
-
-            Assert.assertEquals(3, myNullListener.waitOnCount(3, 500));
-
-            mySpace.stop();
-
-        } catch (Exception anE) {
-            System.err.println("Got exception :(");
-            anE.printStackTrace(System.err);
-        }
-
+        Assert.assertEquals(3, myNullListener.waitOnCount(3, 500));
     }
 
     public static class TestEntry implements Entry {
