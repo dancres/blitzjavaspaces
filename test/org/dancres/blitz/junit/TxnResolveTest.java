63f2808accbaacedef905af03e1f74c998e3c780
Dan Creswell <dan.creswell@gmail.com>
2013-01-09 14:34:54 +0000

    TxnResolveTest becomes part of the junit suite.

--- test/org/dancres/blitz/junit/TxnResolveTest.java
+++ test/org/dancres/blitz/junit/TxnResolveTest.java
@@ -1,4 +1,4 @@
-package org.dancres.blitz.test;
+package org.dancres.blitz.junit;
 
 import junit.framework.Assert;
 import net.jini.core.lease.Lease;
@@ -8,11 +8,13 @@ import net.jini.core.transaction.server.*;
 import org.dancres.blitz.remote.LocalSpace;
 import org.dancres.blitz.remote.LocalTxnMgr;
 
+import org.dancres.blitz.test.DummyEntry;
+import org.dancres.blitz.test.TxnGatewayImpl;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class TxnResolve {
+public class TxnResolveTest {
     private LocalSpace _space;
     private LocalTxnMgr _mgr;
 
